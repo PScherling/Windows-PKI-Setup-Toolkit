402c70860c9857e20f3e41e040a56bdf4cf170b8
Patrick Scherling <scherling.pa@gmail.com>
2025-12-04 12:57:02 +0100

    Update Execute_Revoke_Issued_Cert.bat

--- Source Files/CA_Root_Setup/Execute_Revoke_Issued_Cert.bat
+++ Source Files/CA_Root_Setup/Execute_Revoke_Issued_Cert.bat
@@ -11,5 +11,6 @@
 ::
 ::
 @echo off
-powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\_it\CA_Sub_Setup\1_CA_Managing\3_ca-server_revoke-issued-cert_final.ps1""' -Verb RunAs}"
+powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\_psc\CA_Sub_Setup\1_CA_Managing\3_ca-server_revoke-issued-cert_final.ps1""' -Verb RunAs}"
 pause
+
