e90e2b2af1cf07e563d244b0656d17604d9ae6d7
Patrick Scherling <scherling.pa@gmail.com>
2025-12-04 13:01:44 +0100

    Update Execute_Issue_Cert_Req.bat

--- Source Files/CA_Sub_Setup/Execute_Issue_Cert_Req.bat
+++ Source Files/CA_Sub_Setup/Execute_Issue_Cert_Req.bat
@@ -11,5 +11,6 @@
 ::
 ::
 @echo off
-powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\_it\CA_Sub_Setup\1_CA_Managing\1_ca-server_cert-req-submission_final.ps1""' -Verb RunAs}"
+powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\_psc\CA_Sub_Setup\1_CA_Managing\1_ca-server_cert-req-submission_final.ps1""' -Verb RunAs}"
 pause
+
